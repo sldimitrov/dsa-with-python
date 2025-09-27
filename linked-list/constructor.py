b812984aff300864cae6abcc82f705f57b27286b
Slavi FE <slavidimitrov54@gmail.com>
2025-09-27 16:18:58 +0300

    constructor(linked-list) implement pop method for linked list

--- linked-list/constructor.py
+++ linked-list/constructor.py
@@ -21,29 +21,51 @@ class LinkedList:
             temp = temp.next
 
     def append(self, value):
-        # Create new node
         new_node = Node(value)
-
-        # Set the pointer of the previous node to the current value
-        self.tail.next = new_node
-
-        # Set the tail to the new node & Increase length
-        self.tail = new_node
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node # Set the pointer of the previous node to the current value
+            self.tail = new_node
         self.length += 1
+        return True
 
     def pop(self):
-        # Remove Item
+        if self.length is None:
+            return None
+
+        temp = self.head
+        pre = self.head
+        while temp.next is not None:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        pre.next = None
         self.length -= 1
+
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
 
 # Initialise Linked List
 my_linked_list = LinkedList(4)
 
 # Append a Node
 my_linked_list.append(5)
+my_linked_list.append(5)
+
+print("Print items before pop")
+my_linked_list.print_items()
+
+my_linked_list.pop()
 
 print('Head:', my_linked_list.head.value)
 print('Tail:', my_linked_list.tail.value)
 print('Length:', my_linked_list.length)
 
 # Print Items
+print("Print items after pop")
 my_linked_list.print_items()
+
