12e423a436bf677de0091613c07b9b4283163f88
Slavi FE <slavidimitrov54@gmail.com>
2025-11-05 22:27:39 +0200

    feat(linked-list-methods) implement prepend method to linked list

--- linked-list/constructor.py
+++ linked-list/constructor.py
@@ -31,6 +31,19 @@ class LinkedList:
         self.length += 1
         return True
 
+    def preprend(self, value):
+        new_node = Node(value)
+
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
+
     def pop(self):
         if self.length is None:
             return None
